109973503779d0922169be4787b085afefc21a1a
shabie <30535146+shabie@users.noreply.github.com>
2020-06-27 23:53:06 +0200

    update version to from 4 to X in the string

--- flask-hello.py
+++ flask-hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Hello world! Version 4'
+    return 'Flask Hello world! Version X\n'
 
 @app.route('/test')
 def test():
